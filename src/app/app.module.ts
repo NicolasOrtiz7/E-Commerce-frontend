cea529100670df4844ee85831b376f1e7debb719
Nicolas Ortiz <nicoortiz9870@gmail.com>
2024-01-20 15:07:33 -0300

    navbar and home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { ProductCardComponent } from './component/product-card/product-card.component';
+import { ProductPageComponent } from './component/product-page/product-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    ProductCardComponent,
+    ProductPageComponent
   ],
   imports: [
     BrowserModule,
